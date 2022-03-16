6d01239e0c59fb7d7c9b5e68b4ad6991a4e4fbb0
meisam <maghsoodiradmeisam12@gmail.com>
2022-03-16 16:47:19 +0330

    add contactlist and deleteHandler

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import AddContact from "./components/AddContact/AddContact";
+import ContactList from "./components/ContactList/ContactList";
 
 function App() {
   const [contacts, setContacts] = useState([]);
@@ -13,11 +14,16 @@ function App() {
     ]);
   };
 
+  const deleteContactHandler = (id) => {
+    const filteredContacts = contacts.filter((c) => c.id !== id);
+    setContacts(filteredContacts);
+  };
+
   return (
     <main className="App">
       <h1>Contact App</h1>
       <AddContact addContactHandler={addContactHandler} />
-      <section>Contact List</section>
+      <ContactList contacts={contacts} onDelete={deleteContactHandler} />
     </main>
   );
 }
