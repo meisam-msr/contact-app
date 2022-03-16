01618054e3d3324a7ac4ac1a166cd1217baf48bb
meisam <maghsoodiradmeisam12@gmail.com>
2022-03-16 13:37:51 +0330

    create addcontact cmp

---
+++ src/components/AddContact/AddContact.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+const AddContact = ({ addContactHandler }) => {
+  const [contact, setContact] = useState({ name: "", email: "" });
+
+  const changeHandler = (e) => {
+    setContact({ ...contact, [e.target.name]: e.target.value });
+  };
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    addContactHandler(contact);
+    setContact({ name: "", email: "" });
+  };
+
+  return (
+    <form onSubmit={submitForm}>
+      <div className="formControl">
+        <label>name</label>
+        <input
+          type="text"
+          name="name"
+          value={contact.name}
+          onChange={changeHandler}
+        />
+      </div>
+      <div className="formControl">
+        <label>email</label>
+        <input
+          type="text"
+          name="email"
+          value={contact.email}
+          onChange={changeHandler}
+        />
+      </div>
+      <button type="submit">Add Contact</button>
+    </form>
+  );
+};
+
+export default AddContact;
