b79a09212cdb739e2750c3c1aebda254750013b6
meisam <maghsoodiradmeisam12@gmail.com>
2022-03-18 18:51:57 +0330

    add edit cmp

---
+++ src/components/EditContact/EditContact.js
@@ -0,0 +1,62 @@
+// import "./addContact.css";
+import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect } from "react";
+import getOneContact from "../../services/getOneCOntact";
+
+const EditContact = ({ editContactHandler }) => {
+  const [contact, setContact] = useState({ name: "", email: "" });
+  let navigate = useNavigate();
+  const id = useParams().id;
+
+  const changeHandler = (e) => {
+    setContact({ ...contact, [e.target.name]: e.target.value });
+  };
+
+  const submitForm = (e) => {
+    if (!contact.name || !contact.email) {
+      alert("all fields is mandatory !");
+      return;
+    }
+    e.preventDefault();
+    editContactHandler(contact, id);
+    setContact({ name: "", email: "" });
+    navigate("/");
+  };
+
+  useEffect(() => {
+    const localFetch = async () => {
+      try {
+        const { data } = await getOneContact(id);
+        setContact({ name: data.name, email: data.email });
+      } catch (error) {}
+    };
+    localFetch();
+  }, []);
+
+  return (
+    <form onSubmit={submitForm}>
+      <div className="formControl">
+        <label>name</label>
+        <input
+          type="text"
+          name="name"
+          value={contact.name}
+          onChange={changeHandler}
+        />
+      </div>
+      <div className="formControl">
+        <label>email</label>
+        <input
+          type="text"
+          name="email"
+          value={contact.email}
+          onChange={changeHandler}
+        />
+      </div>
+      <button type="submit">Update Contact</button>
+    </form>
+  );
+};
+
+export default EditContact;
