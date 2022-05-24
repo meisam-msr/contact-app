67fe07a38abf1e53abae63df803ca728bc98c3dd
meisam <maghsoodiradmeisam12@gmail.com>
2022-05-25 00:24:38 +0430

    add endpoint to demo

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3001";
+// this EndPonit just for show data on demo
+// main EndPoint is http://localhost:3001 (json server)
+axios.defaults.baseURL = "https://my-json-server.typicode.com/meisam-msr/contacts-list-server-api/";
 
 const http = {
   get: axios.get,
